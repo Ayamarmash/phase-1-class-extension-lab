7ea634217fd27f2df4b5aeaa29a92830fe496948
root <root@DESKTOP-JFEPESN.localdomain>
2022-11-28 13:22:26 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Polygon{
+    constructor(shapeSides){
+        this.shapeSides = shapeSides
+    }
+    get countSides(){
+        return this.shapeSides.length
+    }
+    get perimeter(){
+        return this.shapeSides.reduce((result, side)=> result + side, 0)
+    }
+}
+class Triangle extends Polygon{
+    get isValid(){
+        let sum = this.perimeter
+        let res = true
+        this.shapeSides.map((side)=>{
+            if((sum - side) < side) res = false
+        })
+        return res
+    }
+}
+class Square extends Polygon{
+    get isValid(){
+        let value = this.shapeSides[0]
+        let res = true
+        for(let i = 1; i < 3; i++){
+            if(this.shapeSides[i] !== value) res = false
+        }
+        return res
+    }
+    get area(){
+        if(this.isValid){
+            return this.shapeSides[0]**2
+        }
+    }
+}
